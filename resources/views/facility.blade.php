9804b9fcbbe0d1fefc83a108bcca25ec96a9c673
SaraRajagukguk <rajagukguksaratesalonika@gmail.com>
2025-05-02 21:19:37 +0700

    update_facility

--- resources/views/facility.blade.php
+++ resources/views/facility.blade.php
@@ -1,156 +1,155 @@
 @include('layouts.main')
 
+<main class="main">
 
-
-    <main class="main">
-
-        <!-- breadcrumb -->
-        <div class="site-breadcrumb" style="background: url(assets/img/breadcrumb/01.jpg)">
-            <div class="container">
-                <h2 class="breadcrumb-title">Our Facilities</h2>
-                <ul class="breadcrumb-menu">
-                    <li><a href="index.html">Home</a></li>
-                    <li class="active">Our Facilities</li>
-                </ul>
-            </div>
+    <!-- breadcrumb -->
+    <div class="site-breadcrumb" style="background: url(assets/img/breadcrumb/01.jpg)">
+        <div class="container">
+            <h2 class="breadcrumb-title">Our Facilities</h2>
+            <ul class="breadcrumb-menu">
+                <li><a href="index.html">Home</a></li>
+                <li class="active">Our Facilities</li>
+            </ul>
         </div>
-        <!-- breadcrumb end -->
+    </div>
+    <!-- breadcrumb end -->
 
-
-
-        <!-- facility area -->
-        <div class="facility-area py-120">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 mx-auto">
-                        <div class="site-heading text-center">
-                            <span class="site-title-tagline"><i class="far fa-book-open-reader"></i> Our Facilities</span>
-                            <h2 class="site-title">Let's Check Our <span>Facilities</span></h2>
-                            <p>It is a long established fact that a reader will be distracted by the readable content of
-                                a page when looking at its layout.</p>
+    <!-- facility area -->
+    <div class="facility-area py-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                    <div class="site-heading text-center">
+                        <span class="site-title-tagline"><i class="fas fa-building"></i> Fasilitas Kami</span>
+                        <h2 class="site-title">Nikmati Berbagai <span>Fasilitas</span> Unggulan Kami</h2>
+                        <p>Kami menyediakan berbagai fasilitas modern dan nyaman untuk mendukung kegiatan akademik,
+                            kesehatan, dan rekreasi mahasiswa.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-lg-4">
+                    <div class="facility-item wow fadeInUp" data-wow-delay=".25s">
+                        <div class="facility-img">
+                            <img src="assets/img/facility/01.jpg" alt="Laboratorium">
+                        </div>
+                        <div class="facility-content">
+                            <h3 class="facility-title">
+                                <a href="#">Laboratorium</a>
+                            </h3>
+                            <p class="facility-text">
+                                Jelajahi lab-lab mutakhir kami yang mendukung eksplorasi mendalam dalam ilmu komputer,
+                                rekayasa perangkat lunak, dan jaringan. Peralatan modern dan bimbingan ahli menanti
+                                Anda.
+                            </p>
+                            <div class="facility-arrow">
+                                <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-6 col-lg-4">
-                        <div class="facility-item wow fadeInUp" data-wow-delay=".25s">
-                            <div class="facility-img">
-                                <img src="assets/img/facility/01.jpg" alt="">
-                            </div>
-                            <div class="facility-content">
-                                <h3 class="facility-title">
-                                    <a href="#">Library Facility</a>
-                                </h3>
-                                <p class="facility-text">
-                                    There are many variations of passages orem psum available but the majority have
-                                    suffer alteration in some form by injected.
-                                </p>
-                                <div class="facility-arrow">
-                                    <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
-                                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="facility-item wow fadeInDown" data-wow-delay=".25s">
+                        <div class="facility-img">
+                            <img src="assets/img/facility/02.jpg" alt="Asrama">
+                        </div>
+                        <div class="facility-content">
+                            <h3 class="facility-title">
+                                <a href="#">Asrama</a>
+                            </h3>
+                            <p class="facility-text">
+                                Rasakan kenyamanan tinggal di asrama yang aman dan suportif. Bangun persahabatan abadi,
+                                manfaatkan lingkungan belajar yang kondusif, dan nikmati kemudahan akses ke fasilitas
+                                kampus.
+                            </p>
+                            <div class="facility-arrow">
+                                <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="facility-item wow fadeInDown" data-wow-delay=".25s">
-                            <div class="facility-img">
-                                <img src="assets/img/facility/02.jpg" alt="">
-                            </div>
-                            <div class="facility-content">
-                                <h3 class="facility-title">
-                                    <a href="#">Hall Facility</a>
-                                </h3>
-                                <p class="facility-text">
-                                    There are many variations of passages orem psum available but the majority have
-                                    suffer alteration in some form by injected.
-                                </p>
-                                <div class="facility-arrow">
-                                    <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
-                                </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="facility-item wow fadeInUp" data-wow-delay=".25s">
+                        <div class="facility-img">
+                            <img src="assets/img/facility/01.jpg" alt="Perpustakaan">
+                        </div>
+                        <div class="facility-content">
+                            <h3 class="facility-title">
+                                <a href="#">Perpustakaan</a>
+                            </h3>
+                            <p class="facility-text">
+                                Benamkan diri Anda dalam dunia pengetahuan di perpustakaan kami yang luas. Temukan
+                                koleksi buku yang beragam, jurnal-jurnal digital terkini, dan ruang belajar yang tenang
+                                untuk fokus pada studi Anda.
+                            </p>
+                            <div class="facility-arrow">
+                                <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="facility-item wow fadeInUp" data-wow-delay=".25s">
-                            <div class="facility-img">
-                                <img src="assets/img/facility/03.jpg" alt="">
-                            </div>
-                            <div class="facility-content">
-                                <h3 class="facility-title">
-                                    <a href="#">Canteen Food Facility</a>
-                                </h3>
-                                <p class="facility-text">
-                                    There are many variations of passages orem psum available but the majority have
-                                    suffer alteration in some form by injected.
-                                </p>
-                                <div class="facility-arrow">
-                                    <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
-                                </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="facility-item wow fadeInUp" data-wow-delay=".25s">
+                        <div class="facility-img">
+                            <img src="assets/img/facility/04.jpg" alt="Kantin">
+                        </div>
+                        <div class="facility-content">
+                            <h3 class="facility-title">
+                                <a href="#">Kantin</a>
+                            </h3>
+                            <p class="facility-text">
+                                Puaskan selera Anda dengan pilihan kuliner lezat dan bergizi di kantin kami. Nikmati
+                                hidangan lokal dan internasional yang terjangkau dalam suasana yang ramah.
+                            </p>
+                            <div class="facility-arrow">
+                                <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="facility-item wow fadeInUp" data-wow-delay=".25s">
-                            <div class="facility-img">
-                                <img src="assets/img/facility/04.jpg" alt="">
-                            </div>
-                            <div class="facility-content">
-                                <h3 class="facility-title">
-                                    <a href="#">Transport Facility</a>
-                                </h3>
-                                <p class="facility-text">
-                                    There are many variations of passages orem psum available but the majority have
-                                    suffer alteration in some form by injected.
-                                </p>
-                                <div class="facility-arrow">
-                                    <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
-                                </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="facility-item wow fadeInDown" data-wow-delay=".25s">
+                        <div class="facility-img">
+                            <img src="assets/img/facility/01.jpg" alt="Klinik">
+                        </div>
+                        <div class="facility-content">
+                            <h3 class="facility-title">
+                                <a href="#">Klinik</a>
+                            </h3>
+                            <p class="facility-text">
+                                Kesehatan Anda adalah prioritas kami. Dapatkan perawatan medis yang cepat dan
+                                berkualitas dari tim medis profesional kami di klinik kampus.
+                            </p>
+                            <div class="facility-arrow">
+                                <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="facility-item wow fadeInDown" data-wow-delay=".25s">
-                            <div class="facility-img">
-                                <img src="assets/img/facility/05.jpg" alt="">
-                            </div>
-                            <div class="facility-content">
-                                <h3 class="facility-title">
-                                    <a href="#">Lab Facility</a>
-                                </h3>
-                                <p class="facility-text">
-                                    There are many variations of passages orem psum available but the majority have
-                                    suffer alteration in some form by injected.
-                                </p>
-                                <div class="facility-arrow">
-                                    <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
-                                </div>
-                            </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="facility-item wow fadeInUp" data-wow-delay=".25s">
+                        <div class="facility-img">
+                            <img src="assets/img/facility/01.jpg" alt="Ruang Terbuka Hijau">
                         </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="facility-item wow fadeInUp" data-wow-delay=".25s">
-                            <div class="facility-img">
-                                <img src="assets/img/facility/06.jpg" alt="">
-                            </div>
-                            <div class="facility-content">
-                                <h3 class="facility-title">
-                                    <a href="#">Gymnasium Facility</a>
-                                </h3>
-                                <p class="facility-text">
-                                    There are many variations of passages orem psum available but the majority have
-                                    suffer alteration in some form by injected.
-                                </p>
-                                <div class="facility-arrow">
-                                    <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
-                                </div>
+                        <div class="facility-content">
+                            <h3 class="facility-title">
+                                <a href="#">Ruang Terbuka Hijau</a>
+                            </h3>
+                            <p class="facility-text">
+                                Temukan ketenangan di ruang terbuka hijau kami yang menyegarkan. Bersantai di tengah
+                                alam, berinteraksi dengan teman, atau sekadar menikmati udara segar.
+                            </p>
+                            <div class="facility-arrow">
+                                <a href="#" class="theme-btn">Read More<i class="fas fa-arrow-right-long"></i></a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- facility area end -->
-        
-    </main>
+    </div>
+    <!-- facility area end -->
 
-    @include('layouts.footer')
+</main>
+
+@include('layouts.footer')
