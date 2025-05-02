7d18df66e071680551e0fd5759123783a0b05ce7
SaraRajagukguk <rajagukguksaratesalonika@gmail.com>
2025-05-02 20:22:51 +0700

    Proyek Akhrir 1
    PA 1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
    <!-- footer area -->
-   <footer class="footer-area">
+<footer class="footer-area" style="background-color: #321979;">
     <div class="footer-shape">
         <img src="assets/img/shape/03.png" alt="">
     </div>
@@ -8,61 +8,46 @@
             <div class="row footer-widget-wrapper pt-100 pb-70">
                 <div class="col-md-6 col-lg-4">
                     <div class="footer-widget-box about-us">
-                        <a href="#" class="footer-logo">
-                            <img src="http://www.del.ac.id/wp-content/uploads/2015/06/logox.png"  alt="D3 Teknologi Informasi">   
-                        </a>
+                        <h4 class="footer-widget-title">Contac Us</h4>
                         <p class="mb-3">
-                            Beberapa informasi mungkin telah mengalami penyesuaian untuk keperluan penyampaian yang menarik,
-                                namun tetap mengutamakan keakuratan dan kredibilitas
-                        </p>
+                            Institut Teknologi Del<br>
+                            Jl. Sisingamangaraja, Sitoluama<br>
+                            Laguboti, Toba Samosir<br>
+                            Sumatera Utara, Indonesia<br>
+                            Kode Pos : 22381<br>
+                            Telp : +62 632 331234
+                          </p>
                         <ul class="footer-contact">
-                            <li><a href="tel:+620632331234"><i class="far fa-phone"></i>0632-331234</a></li>
+                            <li><a href="https://www.facebook.com/share/p/1P52uJ8Wni/" target="_blank"><i class="fab fa-facebook-f"></i>Institut Teknologi Del </a></li>
                             <li><i class="far fa-map-marker-alt"></i>Sitoluama,Laguboti,Toba</li>
-                            <li><a href="mailto:info@example.com"><i
-                                        class="far fa-envelope"></i>diploma3tiDel@gmail.com</a></li>
+                            <li><a href="mailto:info@example.com"><i class="far fa-envelope"></i>info@del.ac.id</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-2">
                     <div class="footer-widget-box list">
-                        <h4 class="footer-widget-title">Quick Links</h4>
+                        <h4 class="footer-widget-title">Site Navigation</h4>
                         <ul class="footer-list">
-                            <li><a href="about.html"><i class="fas fa-caret-right"></i> About Us</a></li>
-                            <li><a href="faq.html"><i class="fas fa-caret-right"></i> FAQ's</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Testimonials</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Terms Of Service</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Privacy policy</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Update News</a></li>
+                            <li><a href="about.html"><i class="fas fa-caret-right"></i> Home</a></li>
+                            <li><a href="faq.html"><i class="fas fa-caret-right"></i> Profil Prodi</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Kurikulum</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Berita</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Pengumuman</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Lectures</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-3">
                     <div class="footer-widget-box list">
-                        <h4 class="footer-widget-title">Our Campus</h4>
+                        <h4 class="footer-widget-title"></h4>
                         <ul class="footer-list">
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Campus Safety</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Student Activities</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Academic Department</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Planning & Administration</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Office Of The Chancellor</a></li>
-                            <li><a href="#"><i class="fas fa-caret-right"></i> Facility Services</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Dokementasi</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> HubConnect</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Prestasi</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Prospek karir</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Alumni</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Fasilitas</a></li>
                         </ul>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="footer-widget-box list">
-                        <h4 class="footer-widget-title">Newsletter</h4>
-                        <div class="footer-newsletter">
-                            <p>Subscribe Our Newsletter To Get Latest Update And News</p>
-                            <div class="subscribe-form">
-                                <form action="#">
-                                    <input type="email" class="form-control" placeholder="Your Email">
-                                    <button class="theme-btn" type="submit">
-                                        Subscribe Now <i class="far fa-paper-plane"></i>
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -74,7 +59,7 @@
                 <div class="row">
                     <div class="col-md-6 align-self-center">
                         <p class="copyright-text">
-                            &copy; Copyright <span id="date"></span> <a href="#"> Eduka </a> All Rights Reserved.
+                            &copy; Copyright <span id="date"></span> <a href="#"> D3 TI © 2025 Institut Teknologi Del. </a> All Rights Reserved.
                         </p>
                     </div>
                     <div class="col-md-6 align-self-center">
